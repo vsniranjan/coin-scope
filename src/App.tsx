12fb75c82742c969ed6259704b44b57c229c56e1
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-10-12 16:49:51 +0530

    Implement coin fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  price_change_percentage_24h: number;
+}
+
 const App = () => {
+  const [coins, setCoins] = useState<Coin[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await axios.get<Coin[]>(API_URL);
+        setCoins(res.data);
+        setLoading(false);
+        console.log(res.data);
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
-      <h1> Coin Scope</h1>
+      <h1>Coin Scope</h1>
     </div>
   );
 };
