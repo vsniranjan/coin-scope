76d56739cc29495c08d9932b1a2fa16083aa1b72
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-10-13 17:59:36 +0530

    Add SortSelector component and integrate sorting functionality in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import axios from "axios";
 
 import CoinCard from "./components/CoinCard";
 import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 import LimitSelector from "./components/LimitSelector";
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -23,6 +24,7 @@ const App = () => {
   const [error, setError] = useState<Error | null>(null);
   const [limit, setLimit] = useState<number>(10);
   const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
 
   // Initial Rendering of coins and updation in changing limit
   useEffect(() => {
@@ -40,15 +42,35 @@ const App = () => {
     };
 
     fetchCoins();
-  }, [limit]);
+  }, [limit, sortBy]);
 
   // Showing Filtered coins
-  const filteredCoins = coins.filter((coin) => {
-    return (
-      coin.name.toLocaleLowerCase().includes(filter.toLowerCase()) ||
-      coin.symbol.toLocaleLowerCase().includes(filter.toLowerCase())
-    );
-  });
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLocaleLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLocaleLowerCase().includes(filter.toLowerCase())
+      );
+    })
+    .slice()
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "market_cap_desc":
+          return b.market_cap - a.market_cap;
+        case "market_cap_asc":
+          return a.market_cap - b.market_cap;
+        case "price_desc":
+          return b.current_price - a.current_price;
+        case "price_asc":
+          return a.current_price - b.current_price;
+        case "change_desc":
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case "change_asc":
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+        default:
+          return 0;
+      }
+    });
 
   return (
     <div>
@@ -58,6 +80,7 @@ const App = () => {
 
       <div className='top-controls'>
         <FilterInput filter={filter} onFilterChange={setFilter} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
         <LimitSelector limit={limit} onLimitChange={setLimit} />
       </div>
 
---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,28 @@
+interface SortSelectorProps {
+  sortBy: string;
+  onSortChange: (value: string) => void;
+}
+
+const SortSelector = ({ sortBy, onSortChange }: SortSelectorProps) => {
+  return (
+    <div className='controls'>
+      <label htmlFor='sort'> Sort By:</label>
+      <select
+        id='sort'
+        value={sortBy}
+        onChange={(e) => onSortChange(e.target.value)}
+      >
+        <option value='market_cap_desc'>Market Cap (High to Low)</option>
+        <option value='market_cap_asc'>Market Cap (Low to High)</option>
+
+        <option value='price_desc'>Price (High to Low)</option>
+        <option value='price_asc'>Price (Low to High)</option>
+
+        <option value='change_desc'>24h Change (High to Low)</option>
+        <option value='change_asc'>24h Change (Low to High)</option>
+      </select>
+    </div>
+  );
+};
+
+export default SortSelector;
